e8fa12cb577d0f5220332ced8d91c397972ba7e1
Hesam Asnaashari <hessamasna@yahoo.com>
2022-11-20 16:08:53 +0330

    [ADD] click listener to courses

--- app/src/main/java/com/cip/TermInator/adapter/CourseAdapter.java
+++ app/src/main/java/com/cip/TermInator/adapter/CourseAdapter.java
@@ -1,10 +1,12 @@
 package com.cip.TermInator.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -33,9 +35,16 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CourseHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CourseHolder holder, @SuppressLint("RecyclerView") int position) {
         Course course = courseArrayList.get(position);
         holder.setData(course);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"clicked"+courseArrayList.get(position).getName(),Toast.LENGTH_SHORT ).show();
+            }
+        });
     }
 
     @Override
