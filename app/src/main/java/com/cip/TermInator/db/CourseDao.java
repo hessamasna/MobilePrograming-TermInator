08645343ff50607d88814f95817e508b78ce8861
Hesam Asnaashari <hessamasna@yahoo.com>
2022-11-23 01:18:36 +0330

    [ADD] Add new Queries

--- app/src/main/java/com/cip/TermInator/db/CourseDao.java
+++ app/src/main/java/com/cip/TermInator/db/CourseDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.cip.TermInator.model.Course;
 
@@ -14,14 +15,23 @@ public interface CourseDao {
     @Query("SELECT * FROM course")
     List<Course> getAllCourses();
 
+    @Query("SELECT * FROM course WHERE uid = :id")
+    List<Course> getCourse(int id);
+
     @Query("SELECT * FROM course WHERE university_faculties LIKE :name")
     List<Course> selectUniversityFaculties(String name);
+
+    @Query("SELECT * FROM course WHERE has_course = 1 ")
+    List<Course> selectHasCourse();
 
     @Insert
     void insertCourse(Course... courses);
 
     @Delete
     void delete(Course course);
+
+    @Update
+    void update(Course course);
 
     @Query("DELETE FROM course")
     void deleteAll();
