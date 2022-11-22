018960681d04f19adfa887e51577685d85ed3ba7
Hesam Asnaashari <hessamasna@yahoo.com>
2022-11-23 01:17:21 +0330

    [ADD] add new Entity

--- app/src/main/java/com/cip/TermInator/db/AppDatabase.java
+++ app/src/main/java/com/cip/TermInator/db/AppDatabase.java
@@ -8,11 +8,13 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.cip.TermInator.model.Course;
+import com.cip.TermInator.model.WeekCourses;
 
-@Database(entities = {Course.class}, version = 1)
+@Database(entities = {Course.class, WeekCourses.class}, version = 5)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract CourseDao courseDao();
+    public abstract WeekCoursesDao weekCoursesDao();
 
     private static AppDatabase INSTANCE;
 
