37af6eaef0f2e13a6712a9cfd30f4bf3ccd38959
Hesam Asnaashari <hessamasna@yahoo.com>
2022-12-02 20:26:13 +0330

    [FIX] add condition to add course

--- app/src/main/java/com/cip/TermInator/model/WeekCourses.java
+++ app/src/main/java/com/cip/TermInator/model/WeekCourses.java
@@ -4,8 +4,17 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.cip.TermInator.db.AppDatabase;
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 public class WeekCourses {
@@ -100,34 +109,41 @@ public class WeekCourses {
         this.day6 = day6;
     }
 
-    public void concatDay(String day, int courseId) {
+    public void concatDay(String day, int courseId, AppDatabase db) {
         switch (day) {
             case "0":
                 this.day0 += courseId + "*";
+                this.day0 = sortCourses(this.day0, db);
                 break;
 
             case "1":
                 this.day1 += courseId + "*";
+                this.day1 = sortCourses(this.day1, db);
                 break;
 
             case "2":
                 this.day2 += courseId + "*";
+                this.day2 = sortCourses(this.day2, db);
                 break;
 
             case "3":
                 this.day3 += courseId + "*";
+                this.day3 = sortCourses(this.day3, db);
                 break;
 
             case "4":
                 this.day4 += courseId + "*";
+                this.day4 = sortCourses(this.day4, db);
                 break;
 
             case "5":
                 this.day5 += courseId + "*";
+                this.day5 = sortCourses(this.day5, db);
                 break;
 
             case "6":
                 this.day6 += courseId + "*";
+                this.day6 = sortCourses(this.day6, db);
                 break;
         }
     }
@@ -144,5 +160,51 @@ public class WeekCourses {
                 ", day5='" + day5 + '\'' +
                 ", day6='" + day6 + '\'' +
                 '}';
+    }
+
+    private String sortCourses(String ids, AppDatabase db) {
+        String[] splitIds = ids.split("\\*");
+        HashMap<String, Double> courseIdWithStartTime = new HashMap<>();
+        Gson gson = new Gson();
+
+        for (String splitId : splitIds) {
+            Course course = db.courseDao().getCourse(Integer.parseInt(splitId));
+            CourseTime[] courseTimes = gson.fromJson(String.valueOf(course.getClass_times()), CourseTime[].class);
+            courseIdWithStartTime.put(splitId, Double.parseDouble(courseTimes[0].getStart()));
+        }
+
+        courseIdWithStartTime = sortByValue(courseIdWithStartTime);
+        String res = "";
+        for (String i : courseIdWithStartTime.keySet()) {
+            res += i + "*";
+        }
+
+        return res;
+    }
+
+    private HashMap<String, Double>
+    sortByValue(HashMap<String, Double> hm) {
+        List<Map.Entry<String, Double>> list
+                = new LinkedList<Map.Entry<String, Double>>(
+                hm.entrySet());
+
+        Collections.sort(
+                list,
+                new Comparator<Map.Entry<String, Double>>() {
+                    public int compare(
+                            Map.Entry<String, Double> object1,
+                            Map.Entry<String, Double> object2) {
+                        return (object1.getValue())
+                                .compareTo(object2.getValue());
+                    }
+                });
+
+        HashMap<String, Double> result
+                = new LinkedHashMap<String, Double>();
+        for (Map.Entry<String, Double> me : list) {
+            result.put(me.getKey(), me.getValue());
+        }
+
+        return result;
     }
 }
